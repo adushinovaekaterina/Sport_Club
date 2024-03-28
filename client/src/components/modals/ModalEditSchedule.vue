f8f1e0878de92859963646aaf7130dc7700349b3
TasyaKh <AkitaSpam@gmail.com>
2024-03-28 23:46:59 +0800

    fix: create new

---
+++ client/src/components/modals/ModalEditSchedule.vue
@@ -0,0 +1,170 @@
+<template>
+    <ModalFull :modal-id="modalId">
+        <template #header> Редактировать расписание</template>
+
+        <template #body>
+
+            <div v-if="cabinetsStore.apiRequest.message || cabinetsStore.apiRequest.error" class="alert alert-warning"
+                 role="alert">
+                {{ cabinetsStore.apiRequest.message }} {{ cabinetsStore.apiRequest.error }}
+            </div>
+            <div class="fw-bold">Аудитория:</div>
+            <v-select
+                    placeholder="Аудитория"
+                    class="v-select"
+                    label="name"
+                    :options="foundAuditories"
+                    v-model="selectedCabinet"
+            ></v-select>
+
+            <!--     time   -->
+            <div class="row g-2 my-2 d-flex justify-content-center text-center">
+                <div class="col-12">
+                    <div class="form-label">Дата</div>
+                    <DatePicker mode="date" v-model="date" hide-time-header/>
+                </div>
+                <div class="col-auto">
+                    <div class="form-label">Дата начала</div>
+                    <DatePicker mode="time" v-model="timeStart"/>
+                </div>
+                <div class="col-auto">
+                    <div class="form-label">Дата окончания</div>
+                    <DatePicker mode="time" v-model="timeEnd"/>
+                </div>
+            </div>
+
+            <div class="row g-2 justify-content-end mt-4">
+                <div class="col-auto">
+                    <button
+                            class="btn-custom-accept"
+                            @click="saveChanges()"
+                    >
+                        Сохранить
+                    </button>
+                </div>
+            </div>
+        </template>
+
+    </ModalFull>
+</template>
+
+<script setup lang="ts">
+import ModalFull from "@/components/modals/ModalFull.vue";
+import {onBeforeMount, ref, watch} from "vue";
+import {DatePicker} from "v-calendar";
+import {useCabinetsTimeStore} from "@/store/schedule/cabinets-time_store";
+import type {ICabinetsTimeEdit} from "@/store/models/schedule/cabinets-time.model";
+import {clone} from "lodash";
+import {useAuditoriesStore} from "@/store/schedule/cabinets_store";
+import type {ICabinet} from "@/store/models/schedule/cabinet.model";
+
+const props = defineProps<{
+    teamId: number; //задать id user
+    scheduleId: number;
+    modalId: string;
+    onSaveChanges: () => void;
+}>();
+
+const cabinetsStore = useCabinetsTimeStore();
+const auditoryStore = useAuditoriesStore();
+
+const date = ref(new Date());
+const timeStart = ref(date.value)
+const timeEnd = ref(date.value);
+
+const foundAuditories = ref<ICabinet[]>([]);
+const selectedCabinet = ref<ICabinet>();
+
+onBeforeMount(() => {
+    date.value.setMinutes(0)
+    date.value.setSeconds(0)
+    editTimeCabinet.value.id_team_schedule = props.scheduleId
+
+    getAuditories()
+});
+
+
+const user = ref();
+const responseMsg = ref("");
+
+const editTimeCabinet = ref<ICabinetsTimeEdit>({});
+
+watch(date, (value) => {
+
+    const tS = clone(value)
+    const tE = clone(value)
+
+    tS.setHours(timeStart.value.getHours())
+    tE.setHours(timeEnd.value.getHours())
+
+    tS.setMinutes(timeStart.value.getMinutes())
+    tE.setMinutes(timeEnd.value.getMinutes())
+
+    timeStart.value = tS
+    timeEnd.value = tE
+})
+
+watch(
+    () => props.scheduleId,
+    async () => {
+        editTimeCabinet.value.id_team_schedule = props.scheduleId
+    },
+);
+
+// async function fetchS() {
+//     const res = await userStore.getUser(props.userId);
+//     user.value = res.data;
+// }
+
+async function getAuditories() {
+    let r = await auditoryStore.getCabinets({});
+    foundAuditories.value = r.cabinets;
+}
+
+async function saveChanges() {
+    await addTime()
+}
+
+async function addTime() {
+    editTimeCabinet.value.repeat = true
+    editTimeCabinet.value.date = date.value
+    editTimeCabinet.value.id_cabinet = selectedCabinet.value?.id
+    editTimeCabinet.value.time_start = timeStart.value.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    })
+    editTimeCabinet.value.time_end = timeEnd.value.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    })
+
+    await cabinetsStore
+        .setCabinetsTime(editTimeCabinet.value)
+        .then(async () => {
+            props.onSaveChanges();
+        })
+        .catch((err) => {
+            responseMsg.value = err;
+        });
+}
+
+</script>
+
+<style lang="scss" scoped>
+.create-perm {
+  border: var(--main-border-card);
+  border-radius: 50px;
+  overflow: hidden;
+
+  input {
+    border: none;
+
+    &:hover, &:focus, &:active {
+      border: none;
+      box-shadow: none;
+    }
+  }
+}
+</style>
